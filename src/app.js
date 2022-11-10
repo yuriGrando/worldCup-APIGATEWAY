8414addb685d99e19f26c65e3892d17535cfd984
leandrodetogni <leandro.togni@sparkag.com.br>
2022-11-10 15:25:17 -0300

    Adicionando o axios e fazendo o request na API

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ const app = express();
 const router = require('./router')
 const cors = require('cors');
 const midlleware = require('./middleware');
+const axios = require('axios');
 
 app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"],
     credentials: true
 }))
+
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzY5MDljMTZjYWNjMDZmNDhlYWVkM2QiLCJpYXQiOjE2NjgwNDEyMTcsImV4cCI6MTY2ODEyNzYxN30.4w_uIllu4_E6b-IYd_-EK0duBZysPTgWNGzZ8PfnFfY'
+
+axios.defaults.headers.common = { 'Authorization': 'Bearer ' + token };
 
 app.use(router);
 
--- src/router.js
+++ src/router.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-// ======= IMPORTS JSON ========
-const match = require("./data/match.json");
-const team = require("./data/team.json");
-const standings = require("./data/standings.json");
+let teamResponse;
 
 // ======= REQ ========
-router.get('/match', (req, res) => res.status(200).send(res.json(match)));
-
-router.get('/team', (req, res) => res.status(200).send(res.json(team)));
-
-router.get('/match', (req, res) => res.status(200).send(res.json(standings)));
+router.get('/team', (req, res) => {
+    axios({
+        method: 'get',
+        url: 'http://api.cup2022.ir/api/v1/team',
+    }).then(function (response) {
+        teamResponse = response.data;
+    });
+    res.status(200).send(teamResponse);
+});
 
 module.exports = router
