ea1251657225417d198296b137d5d459b41607f8
VitorGrigoloo <vitorgrigoloo@gmail.com>
2022-11-17 19:17:07 -0300

    Adicionando gateway copa do mundo

--- src/app.js
+++ src/app.js
@@ -2,58 +2,20 @@ const express = require('express')
 const app = express();
 const router = require('./router')
 const cors = require('cors');
-const axios = require('axios');
-const cron = require('node-cron');
+
+const wordlGateway = require('./worldcup-gateway');
+const { tryJwtExpiration } = require('./utils/token.util')
+
 
 app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"],
     credentials: true
-}))
+}));
 
-let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzY5MDljMTZjYWNjMDZmNDhlYWVkM2QiLCJpYXQiOjE2NjgxMjk4NDIsImV4cCI6MTY2ODIxNjI0Mn0.FEtUJEBvH2hmkAxVu5sluYS2M3Llh0Egfur4cxC34Mc'
-let login = {
-    email: 'yv.grando@gmail.com',
-    password: '1o*18*v9*'
-}
-
-const getToken = cron.schedule('* 29 22 * * *', () => {
-    console.log('oi')
-    axios.post('http://api.cup2022.ir/api/v1/user/login', login).then((res)=>{
-        console.log('OK')
-        token = res.data.data.token;
-        console.log(token)
-        getToken.stop()
-    }).catch((err)=>{
-        console.log('erro')
-        console.log(err)
-    })
-
-}, {
-    scheduled: true,
-    timezone: "America/Sao_Paulo"
-});
-
-// function getToken() {
-//     return (async () => {
-//         try {
-//             await axios.post('http://api.cup2022.ir/api/v1/user/login', login).then((res)=>{
-//                 console.log('OK')
-//                 token = res.data.data.token;
-//                 console.log(token)
-//                 stop = true;
-//             })
-//
-//         } catch (error) {
-//             return error.response.status;
-//         }
-//     })();
-// }
-
-axios.defaults.headers.common =
-    {'Authorization': `Bearer ${token}`}
-;
+tryJwtExpiration();
 
 app.use(router);
+app.use("/wordlcup", wordlGateway);
 
 module.exports = app;
---
+++ src/worldcup-gateway.js
@@ -0,0 +1,49 @@
+const router = require('express').Router()
+const request = require('request-promise-native');
+const axios = require("axios");
+const { getToken } = require('./utils/token.util');
+
+function handleAuthErrors(response, options) {
+    try {
+        getToken().then(async (resp) => {
+            const res = await request(options);
+            response.json(res);
+        }).catch((err) => {
+            response.statusCode(500).send(err);
+        });
+    } catch (err) {
+        response.send(err);
+    }
+}
+
+router.all('*', async (req, res, next) => {
+    const path = req.originalUrl.slice(9);
+    const uri = `http://api.cup2022.ir${path}`;
+    console.log(axios.defaults.headers.common);
+
+    /** @type {request.RequestPromiseOptions} options */
+    const options = {
+        headers: axios.defaults.headers.common,
+        uri: uri,
+        method: req.method,
+    };
+
+    try {
+        const response = await request(options);
+        res.json(response);
+    } catch (err) {
+        if (err.statusCode) {
+            if (err.statusCode == 404) return handleAuthErrors(res, options);
+
+            return res.status(err.statusCode).send({
+                message: err.message
+            });
+        }
+        return res.status(400).send({
+            message: "Ocorreu um erro, fodase",
+            data: err
+        });
+    }
+})
+
+module.exports = router;
